07bc53e37649a4061f2d2fea8b0c679410199353
OriShefer <ori150403@gmail.com>
2023-05-09 14:31:47 +0300

    set rem instead of px where needed

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     
     <div className="container">
-      <img src={process.env.PUBLIC_URL + 'img/user.png'} width="50" height="50"/> 
+      <img src={process.env.PUBLIC_URL + 'img/user.png'} width="50rem" height="50rem"/> 
       <div>
       <h1>Good Morning, Ori</h1>
       <h4>Welcome back, nice to see you again!</h4>
--- frontend/src/Components/Sidebar/Sidebar.js
+++ frontend/src/Components/Sidebar/Sidebar.js
@@ -3,41 +3,40 @@ import "./Sidebar.css";
 
 function Sidebar() {
   return (
-    
-<div class="d-flex flex-column p-3 bg-light position-fixed" style={{width: "280px", height: "2000px"}}>
+<div class="d-flex flex-column p-3 bg-light position-fixed" style={{width: "16rem", height: "100%"}}>
     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-    <img class="m-2" src={process.env.PUBLIC_URL + 'img/piggy-bank.png'} width="40" height="35"/> 
+    <img class="m-2" src={process.env.PUBLIC_URL + 'img/piggy-bank.png'} width="40rem" height="35rem"/> 
       <span class="fs-4">Money Tracker</span>
     </a>
     <hr/>
     <ul class="nav nav-pills flex-column mb-auto">
       <li class="nav-item">
         <a href="#" class="nav-link active" aria-current="page">
-        <img class="me-2" src={process.env.PUBLIC_URL + 'img/piggy-bank.png'} width="16" height="16"/> 
+        <img class="me-2" src={process.env.PUBLIC_URL + 'img/piggy-bank.png'} width="16rem" height="16rem"/> 
           Home
         </a>
       </li>
       <li>
         <a href="#" class="nav-link link-dark">
-        <img class="me-2" src={process.env.PUBLIC_URL + 'img/piggy-bank.png'} width="16" height="16"/> 
+        <img class="me-2" src={process.env.PUBLIC_URL + 'img/piggy-bank.png'} width="16rem" height="16rem"/> 
           Dashboard
         </a>
       </li>
       <li>
         <a href="#" class="nav-link link-dark">
-        <img class="me-2" src={process.env.PUBLIC_URL + 'img/piggy-bank.png'} width="16" height="16"/> 
+        <img class="me-2" src={process.env.PUBLIC_URL + 'img/piggy-bank.png'} width="16rem" height="16rem"/> 
           Orders
         </a>
       </li>
       <li>
         <a href="#" class="nav-link link-dark">
-        <img class="me-2" src={process.env.PUBLIC_URL + 'img/piggy-bank.png'} width="16" height="16"/> 
+        <img class="me-2" src={process.env.PUBLIC_URL + 'img/piggy-bank.png'} width="16rem" height="16rem"/> 
           Products
         </a>
       </li>
       <li>
         <a href="#" class="nav-link link-dark">
-        <img class="me-2" src={process.env.PUBLIC_URL + 'img/piggy-bank.png'} width="16" height="16"/> 
+        <img class="me-2" src={process.env.PUBLIC_URL + 'img/piggy-bank.png'} width="16rem" height="16rem"/> 
           Customers
         </a>
       </li>
