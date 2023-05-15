bdbc324effe1472d10db7db4c87774abe08d421f
OriShefer <ori150403@gmail.com>
2023-05-15 23:12:20 +0300

    validation check and number of chart limit

--- frontend/src/Components/TransactionForm/TransactionForm.js
+++ frontend/src/Components/TransactionForm/TransactionForm.js
@@ -1,5 +1,5 @@
 
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 import "./TransactionForm.css";
 
 function reducer(state, action) {
@@ -37,6 +37,12 @@ function TransactionsForm() {
         description: '',
         category: ''
     });
+    
+    const [valid,setValid] = useState({
+      titleValid: true,
+      amountValid: true,
+      categoryValid: true
+    })
 
     const changeHandler = (e) => {
       e.preventDefault()
@@ -44,6 +50,7 @@ function TransactionsForm() {
       switch (e.target.id) {
         case 'Title':
           dispatch({ type: 'changeTitle', payload: e.target.value})
+          console.log(e.target)
           break;
           case 'Amount':
             dispatch({ type: 'changeAmount', payload: e.target.value})
@@ -65,13 +72,17 @@ function TransactionsForm() {
 
       const [title,amount,description,category] = [e.target.Title.value,e.target.Amount.value,e.target.Description.value,e.target.Category.value]
 
-      if(!title || !amount || category == 'Choose...')
-      {
-        console.log('all fields')
-      }
+    
+        setValid({
+          titleValid: title? true: false,
+          amountValid: amount? true: false,
+          categoryValid: category!= 'Choose...'? true: false,
+
+        })
+      
 
     }
-
+    
 
   return (
 
@@ -79,40 +90,29 @@ function TransactionsForm() {
           <form onSubmit={submitHandler} className="needs-validation" noValidate="">
           <div className="col-md-4 mb-3">
                 <label htmlFor="Title">Title</label>
-                <input onChange={changeHandler} type="text" className="form-control" name="tosic" id="Title" value={state.title} required=""/>
-                <div className="invalid-feedback">
-                  Valid Title is required.
-                </div>
-              </div>
+                <input onChange={changeHandler} type="text" className={valid.titleValid? 'form-control': "form-control invalid"} name="tosic" id="Title" value={state.title} required=""/>
+            </div>
 
             <div className="col-md-4 mb-3">
               <label htmlFor="Amount">Amount</label>
               <div className="input-group">
-                <input onChange={changeHandler} type="number" className="form-control" id="Amount" min={0}  value={state.amount} required=""/>
-                <div className="invalid-feedback" style={{width: '100%'}}>
-                Amount is required.
-                </div>
+                <input onChange={changeHandler} type="number" className={valid.amountValid? 'form-control': "form-control invalid"} id="Amount" min={0}  value={state.amount} required=""/>
               </div>
             </div>
 
             <div className="col-md-4 mb-3">
               <label htmlFor="Description">Description <span className="text-muted">(Optional)</span></label>
               <input onChange={changeHandler} type="test" className="form-control" id="Description" value={state.description}/>
-              <div className="invalid-feedback">
-                Please enter a valid Description
-              </div>
+
             </div>
 
             <div className="row">
               <div className="col-md-4 mb-3">
                 <label htmlFor="Category">Category</label>
-                <select onChange={changeHandler} className="custom-select d-block w-100" id="Category" value={state.category} required="">
+                <select onChange={changeHandler} className={valid.categoryValid? "custom-select d-block w-100": "custom-select d-block w-100 invalid"} id="Category" value={state.category} required="">
                   <option>Choose...</option>
                   <option>United States</option>
                 </select>
-                <div className="invalid-feedback">
-                  Please select a valid country.
-                </div>
               </div>
             </div>
 
